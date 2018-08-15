548d32102b79b93da58cecef337b2b7520a3452e
md mia <sharifmia091@gmail.com>
2018-08-15 06:09:16 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -2,11 +2,11 @@
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
   if (number % 3 == 0) && (number % 5 == 0)
-    "FizzBuzz"
+    puts"FizzBuzz"
   elsif number % 5 == 0
-    "Buzz"
+    puts"Buzz"
   elsif number % 3 == 0
-    "Fizz"
+    puts"Fizz"
   end
 end
 fizzbuzz(15)
