0f2eca5e3bcf80f7e20241edacffd0eff2914a31
md mia <sharifmia091@gmail.com>
2018-08-15 06:15:32 +0000

    automatically backed up by learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -1,11 +1,18 @@
 # Don't forget! This file needs to be 'required' in its spec file
 # See README.md for instructions on how to do this
 def fizzbuzz(number)
-  if (number % 3 == 0) && (number % 5 == 0)
-    "FizzBuzz"
-  elsif number % 5 == 0
-    "Buzz"
-  elsif number % 3 == 0
-    "Fizz"
-  end
+    divisibleBy3 = (number % 3 == 0)
+    divisibleBy5 = (number % 5 == 0)
+
+    case
+        when divisibleBy3 && divisibleBy5
+            return "FizzBuzz"
+        when divisibleBy3
+            return "Fizz"
+        when divisibleBy5
+            return "Buzz"
+        else 
+            return "Nill"
+    end
+
 end
